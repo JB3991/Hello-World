55c002389d7a78afc5e2d066b8453aeddb65e6fe
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2020-11-18 19:05:00 +0000

    First iOS App
    Hello World App

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        label.text = "Hello World 🌎"
     }
 
 
